fe1b349bf3a663d102af309db7d6999e60c04ac8
Shashmit kumar <shaashmit@gmail.com>
2023-11-08 06:41:37 +0530

    new route to update

--- src/routes/board.js
+++ src/routes/board.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { body } = require("express-validator");
-const { param } = require('express-validator')
+const { param } = require("express-validator");
 const validation = require("../handlers/validation");
 const tokenHandler = require("../handlers/tokenHandler");
 const boardController = require("../controllers/board");
@@ -12,6 +12,17 @@ router.get("/", tokenHandler.verifyToken, boardController.getAll);
 router.put("/", tokenHandler.verifyToken, boardController.updatePostion);
 
 router.get(
+  "/:boardId",
+  param("boardId").custom((value) => {
+    if (!validation.isObjectId(value)) {
+      return Promise.reject("invalid id");
+    } else return Promise.resolve();
+  }),
+  validation.validate,
+  tokenHandler.verifyToken,
+  boardController.getOne
+);
+router.put(
   "/:boardId",
   param("boardId").custom((value) => {
     if (!validation.isObjectId(value)) {
